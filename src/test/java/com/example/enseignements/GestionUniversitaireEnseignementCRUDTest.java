4610ae2f6c5fa7a0ee12c5d3308a52ff2ddb15cd
assfrdz <assia.ferradj.sio@gmail.com>
2025-11-26 15:22:31 +0100

    nn

--- src/test/java/com/example/enseignements/GestionUniversitaireEnseignementCRUDTest.java
+++ src/test/java/com/example/enseignements/GestionUniversitaireEnseignementCRUDTest.java
@@ -1,10 +1,11 @@
+package test.java.com.example.enseignements;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.sql.SQLException;
-
-import org.junit.jupiter.api.Test;
 
 class GestionUniversitaireEnseignementCRUDTest {
 
---
+++ src/test/java/com/example/enseignements/test.java
@@ -0,0 +1,19 @@
+package test.java.com.example.enseignements;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.sql.SQLException;
+
+public class test {
+
+    @BeforeEach
+    void enseignementDAO(){
+
+        
+
+    }
+    
+}
